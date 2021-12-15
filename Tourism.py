97730593fa6465eaa87c7705ccdb2f79eccafc92
kkafawaz <95983756+kkafawaz@users.noreply.github.com>
2021-12-15 19:30:25 +0000

    Update Tourism.py

--- Tourism.py
+++ Tourism.py
@@ -1,20 +1,78 @@
-pragma solidity ^0.4.23;
+// Solidity program to demonstrate
+// visibility modifiers
+pragma solidity ^0.5.0;
 
 // Creating a contract
-contract Test {
+contract contract_example {
+
+// Declaring private
+// state variable
+uint private num1;
 	
-// Declaring variable
-string str;
+// Declaring public
+// state variable
+uint public num2;
+
+// Declaring Internal
+// state variable
+string internal str;
 
 // Defining a constructor
-constructor(string str_in){
-	str = str_in;
+constructor() public {
+	num2 = 10;
 }
 
-// Defining a function to
-// return value of variable 'str'
-function str_out(
-) public view returns(string memory){
-	return str;
+// Defining a private function
+function increment(
+	uint data1) private pure returns(
+	uint) { return data1 + 1; }
+	
+// Defining public functions
+function updateValue(
+	uint data1) public { num1 = data1; }
+function getValue(
+) public view returns(
+	uint) { return num1; }
+
+// Declaring public functions
+function setStr(
+	string memory _str) public;
+function getStr(
+) public returns (string memory);
+}
+
+// Child contract inheriting
+// from the parent contract
+// 'contract_example'
+contract derived_contract is contract_example{
+
+// Defining public function of
+// parent contract
+function setStr(
+	string memory _str) public{
+str = _str;
+}
+
+// Defining public function
+// of parent contract
+function getStr(
+) public returns (
+	string memory){ return str; }
+}
+
+//External Contract
+contract D {
+
+// Defining a public function to create
+// an object of child contract access the
+// functions from child and parent contract
+function readData(
+) public payable returns(
+	string memory, uint) {
+	contract_example c
+		= new derived_contract();
+	c.setStr("GeeksForGeeks");
+	c.updateValue(16);		
+	return (c.getStr(), c.getValue());
 }
 }
